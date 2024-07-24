0547733f9f264dfe2bb4d992bbe9a4b34fbe219c
rjprattes <85262485+rjprattes@users.noreply.github.com>
2024-07-24 11:51:56 -0300

    Initial commit

---
+++ src/test/java/me/dio/DioJavaRestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.dio_java_restfull_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioJavaRestfullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
